fa243d587f06cd7f009eff5b762da0ad5aaf803c
joyme123 <jiangpengfei12@gmail.com>
2016-11-14 23:53:12 +0800

    add document parser

---
+++ core/util/DocumentParser.h
@@ -0,0 +1,20 @@
+#ifndef DOCUMENTPARSER_H
+#define DOCUMENTPARSER_H
+#include<iostream>
+#include<string>
+
+#include"../model/InvertedIndexHash.h"
+using namespace std;
+
+/**
+ * Document Parser,aid to parser document to invertedHashIndex
+ * author jiangpengfei12@gmail.com
+ * 2016-11-14
+ */
+class DocumentParser{
+    vector<InvertedIndexHash> invertedHashIndex;            // invertedHashIndex array
+public:
+    
+    
+};
+#endif
