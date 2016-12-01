290c336781404e92ee66b0bd4c277c4fba3768ea
joyme123 <jiangpengfei12@gmail.com>
2016-12-01 20:24:52 +0800

    同步一下

--- core/util/SocketStreamParser.cpp
+++ core/util/SocketStreamParser.cpp
@@ -2,4 +2,28 @@
  * SocketStreamParser的实现
  */
 
- 
+#include "SocketStreamParser.h"
+#include<string>
+
+std::vector<Package> SocketStreamParser::parse(std::string &data){
+    std::vector<Package> packs;
+    char v = data[0];
+    unsigned int strLen = data.length();
+
+    Package pack;
+    string body;
+    unsigned int length;
+    //校验版本号
+    if(v == version){
+        length = data[1];
+        length = length<<8+data[2];
+        length = length<<8+data[3];         //计算出length的大小
+    }
+
+    //可以截取到一个数据包
+    if(strLen - 4 >= length){
+        body = data.substr()
+    }
+
+    return packs;
+}
--- core/util/SocketStreamParser.h
+++ core/util/SocketStreamParser.h
@@ -11,27 +11,15 @@
 #include"Package.h"
 
 class SocketStreamParser{
-    private:
-        const int version = 1;     //版本号为1
-        unsigned int length;       //body部分的长度
-
-        /**
-         * 解析数据流的头部
-         */
-        private bool parseHeader();
-
-        /**
-         * 解析数据流的body
-         */
-        private void parseBody();
     public:
+        static char version = 0x01;
 
         /**
          * 传入接收到的数据包进行解析
          * @param data 要传入的数据
          * @return 返回package数组，并且data变成数据流解析后剩下的部分
          */
-        public vector<Package> parse(string& data);
+        static std::vector<Package> parse(std::string& data);
 
 
  }
